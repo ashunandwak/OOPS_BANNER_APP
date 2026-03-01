74f7153760c7f20a69a6f1870a6b1d16f1a26e68
ashunandwak <as8811@srmist.edu.in>
2026-03-01 23:44:46 +0530

    Create OOPS_banner.java

---
+++ OOPS_banner.java
@@ -0,0 +1,14 @@
+public class OOPS_BANNER_APP {
+    /**
+     * UC1 - print the literal text "OOPS".  This is the simplest
+     * entry point to the application, demonstrating class and
+     * main-method syntax along with a basic output statement.
+     *
+     * @param args command line arguments (ignored)
+     * @author Shivam Kumar
+     * @version 1.0
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
